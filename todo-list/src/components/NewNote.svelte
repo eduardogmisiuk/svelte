6b23d0e3d0a3cd3d32861904628d1ff72403ae45
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2023-09-24 20:05:55 -0300

    [todo-list] Add NewNote component to create notes inside the notes list

---
+++ todo-list/src/components/NewNote.svelte
@@ -0,0 +1,71 @@
+<!--suppress HtmlUnknownTag -->
+
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  let isCreatingNewNote = false;
+  let noteText = "";
+
+  const dispatch = createEventDispatcher();
+
+  function createNewNote() {
+    if (!isCreatingNewNote) {
+      isCreatingNewNote = true;
+    }
+  }
+
+  function handleEnter() {
+    if (noteText && noteText !== "") {
+      dispatch("new-note", { text: noteText });
+      noteText = "";
+      isCreatingNewNote = false;
+    }
+  }
+
+  function handleEscape() {
+    noteText = "";
+    isCreatingNewNote = false;
+  }
+
+  const keyHandlers = {
+    "Enter": handleEnter,
+    "Escape": handleEscape
+  };
+
+  function sendNoteToParent(pressedKey) {
+    const handler = keyHandlers[pressedKey];
+    if (handler) {
+      handler();
+    }
+  }
+</script>
+
+<div aria-hidden="true"
+     on:click={createNewNote}
+     on:keydown={(event) => sendNoteToParent(event.key)}
+     class="text-gray-400 hover:text-gray-700 transition"
+>
+  {#if (isCreatingNewNote)}
+    <!-- Even though ESLint does not like autofocus, the reason given by them doesn't seem to apply in this case -->
+    <!-- svelte-ignore a11y-autofocus -->
+    <input id="note-text-input"
+           type="text"
+           autofocus="autofocus"
+           bind:value={noteText}
+           class="border-2 rounded py-1 px-2 transition"
+    />
+    <iconify-icon aria-hidden="true"
+                  on:click={handleEnter}
+                  icon="ei:check"
+                  class="text-3xl align-middle"
+    />
+    <iconify-icon aria-hidden="true"
+                  on:click|stopPropagation={handleEscape}
+                  icon="ei:close-o"
+                  class="text-3xl align-middle"
+    />
+  {:else}
+    <iconify-icon icon="ei:plus" class="text-3xl align-middle" />
+    <span class="align-middle">Create new note...</span>
+  {/if}
+</div>
--- todo-list/src/routes/+page.svelte
+++ todo-list/src/routes/+page.svelte
@@ -1,32 +1,31 @@
 <script>
-	import TodoItem from '../models/TodoItem.js';
+  import TodoItem from "../models/TodoItem.js";
+  import NewNote from "../components/NewNote.svelte";
 
-	let items = [];
-	for (let i = 1; i <= 30; i++) {
-		items.push(new TodoItem(i));
-	}
+  let items = [];
 
-	function addNewTodo() {
-		items = [...items, new TodoItem(items.length + 1)];
-	}
+  function addNewTodo(text) {
+    items = [...items, new TodoItem(items.length + 1, text)];
+  }
 </script>
 
 <div class="content-center flex flex-col gap-3">
-	<button
-		class="bg-sky-300 items-center rounded-xl self-center px-1.5 py-1 border-black border-2"
-		on:click={addNewTodo}
-	>
-		Add new item
-	</button>
+  <!--  <button-->
+  <!--    class="bg-sky-300 items-center rounded-xl self-center px-1.5 py-1 border-black border-2"-->
+  <!--    on:click={addNewTodo}-->
+  <!--  >-->
+  <!--    Add new item-->
+  <!--  </button>-->
 
-	<div
-		class="max-h-96 flex content-center flex-col bg-white overflow-y-auto border-solid border-2 border-black rounded-xl p-2 gap-1"
-	>
-		{#each items as item (item.id)}
+  <div
+    class="max-h-96 flex content-center flex-col bg-white overflow-y-auto border-solid border-2 border-black rounded-xl p-2 gap-1"
+  >
+    <NewNote on:new-note={(event) => addNewTodo(event.detail.text)} />
+    {#each items as item (item.id)}
 			<span>
-				<input type="checkbox" bind:checked={item.isChecked} />
-				{item.text}
+				<input type="checkbox" bind:checked={item.isChecked} class="rounded" />
+        {item.text}
 			</span>
-		{/each}
-	</div>
+    {/each}
+  </div>
 </div>
