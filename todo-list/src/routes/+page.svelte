b6d2c345d3eed3d9eb17ec4d21f83f4480d2ef9e
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2023-09-26 23:21:09 -0300

    [todo-list] Change heights of lists to be dynamically independent of each other

--- todo-list/src/routes/+page.svelte
+++ todo-list/src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script>
-  import Todo from "../components/lists/TodoList.svelte";
+  import TodoList from "../components/lists/TodoList.svelte";
   import DoneList from "../components/lists/DoneList.svelte";
 </script>
 
 <div class="flex justify-evenly gap-5">
-  <Todo />
-  <DoneList />
+
+  <div class="w-full">
+    <TodoList />
+  </div>
+
+  <div class="w-full">
+    <DoneList />
+  </div>
 </div>
