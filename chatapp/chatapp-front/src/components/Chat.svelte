92baba2eaf74bb287a2c99848fff8a7d3df28578
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2023-10-08 14:46:48 -0300

    [chatapp] Style scrollbar

--- chatapp/chatapp-front/src/components/Chat.svelte
+++ chatapp/chatapp-front/src/components/Chat.svelte
@@ -7,11 +7,11 @@
 
 <div class='
 		flex flex-col
-		rounded-2xl
+		rounded-xl
 		m-4 p-5 gap-2
 		bg-white
 		max-h-[97vh]
-		overflow-auto
+		overflow-y-auto
 	'
 >
 	{#each $messages as message (message.id)}
